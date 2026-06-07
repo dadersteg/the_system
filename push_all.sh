a3de4c67f962a8a55c2b2b1a9003285e8efaa6cc
dadersteg <adersteg.daniel@gmail.com>
2026-06-07 11:59:47 +0100

    Fix: Set Private project as default deployment target

--- push_all.sh
+++ push_all.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 echo "Pushing to Work Project..."
 cp .clasp-work.json .clasp.json
-npx clasp push
+npx clasp push --force
 echo "Pushing to Private Project..."
 cp .clasp-private.json .clasp.json
-npx clasp push
-echo "Restoring Work Project as default..."
-cp .clasp-work.json .clasp.json
+npx clasp push --force
+echo "Restoring Private Project as default..."
+cp .clasp-private.json .clasp.json
 echo "Done!"
